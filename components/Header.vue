522809d13a8a74622a2a315d5bdb3676632e30c9
Arnie <arnie752@gmail.com>
2024-02-27 22:01:30 +0300

    feat(header): Added a Header Component
    Installed the nuxt-icon Module

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <header>
-      <nav class="container mx-auto p-4 flex flex-row justify-between font-montserrat text-2xl">
+      <nav class="sticky container mx-auto p-6 flex flex-row justify-between font-montserrat text-2xl">
         <div class="flex flex-row gap-10 items-baseline">
-          <NuxtLink to="/" class="font-title text-[50px]">luxe</NuxtLink>
-          <NuxtLink to="/products">Products</NuxtLink>
+          <NuxtLink to="/" class="font-title text-[60px] text-gold font-bold">luxe</NuxtLink>
+          <NuxtLink to="/products">Men</NuxtLink>
+          <NuxtLink to="/products">Women</NuxtLink>
           <NuxtLink to="/about">About</NuxtLink>
         </div>
-        <div>
-          <!-- TODO :: Add Icons Here -->
+        <div class="flex flex-row gap-4 items-baseline">
+          <NuxtLink to="/"><Icon name="ic:outline-help-outline" size="2.5rem"/></NuxtLink>
+          <NuxtLink to="/"><Icon name="ic:outline-account-circle" size="2.5rem"/></NuxtLink>
+          <NuxtLink to="/"><Icon name="ic:outline-shopping-cart" size="2.5rem"/></NuxtLink>
         </div>
       </nav>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: [],
   theme: {
+    colors: {
+      'gold': '#CBAB2A',
+      'onyx': '#393E41',
+      'artichoke': '#90A583',
+      'cadet_blue': '#66999B',
+      'red': '#E84855'
+    },
     fontFamily: {
       sans: ["Comfortaa", "sans-serif"],
       heading: ["Montserrat", "sans-serif"],

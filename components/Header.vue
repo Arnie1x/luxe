8d2db8e635026ef716e41311ebb789ec02a24a39
Arnie <arnie752@gmail.com>
2024-02-27 05:54:19 +0300

    feat(tailwind)!: Imported custom fonts for the application.
    Also setup the basic Header Component.

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <header>
-      <p>Header</p>
+      <nav class="container mx-auto p-4 flex flex-row justify-between font-montserrat text-2xl">
+        <div class="flex flex-row gap-10 items-baseline">
+          <NuxtLink to="/" class="font-title text-[50px]">luxe</NuxtLink>
+          <NuxtLink to="/products">Products</NuxtLink>
+          <NuxtLink to="/about">About</NuxtLink>
+        </div>
+        <div>
+          <!-- TODO :: Add Icons Here -->
+        </div>
+      </nav>
     </header>
   </div>
 </template>
@@ -10,6 +19,4 @@
 
 </script>
 
-<style>
-
-</style>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: [],
   theme: {
+    fontFamily: {
+      sans: ["Comfortaa", "sans-serif"],
+      heading: ["Montserrat", "sans-serif"],
+      title: ['"Dancing Script"', "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
